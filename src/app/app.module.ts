1197c106d298978ab75ce525b233330ca91fe76a
jdjuan <david.juanherrera@gmail.com>
2017-07-29 08:29:49 -0500

    add reactive extension

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { StoreModule } from "@ngrx/store";
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { StoreModule } from "@ngrx/store";
   imports: [
     BrowserModule,
     FormsModule,
-    StoreModule.forRoot(reducers)
+    StoreModule.forRoot(reducers),
+    StoreDevtoolsModule.instrument()
   ],
   providers: [],
   bootstrap: [AppComponent]
